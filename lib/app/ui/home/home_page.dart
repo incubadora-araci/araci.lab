e41bb2d1f5a03d6f62983ca0e18d81d597312fef
Lucas de Castro Lopes <lucascastro@id.uff.br>
2020-11-23 12:54:09 -0300

    [0.0.1] Added markdown plugin

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -2,16 +2,22 @@ import 'package:araci/app/controller/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home Page')),
+      appBar: AppBar(title: Text('Home Page')), 
       body: Container(
-        child: Center(
-          child: Text("Home Page"),
-        ),
+        child: markdown("# Bem-vindo ao Araci!"+
+        "\nEste é um exemplo de texto em Markdown.  "+
+        "\nTexto em **negrito**, texto em *itálico*"+
+        "\n - Tópico 1  "+
+        "\n - Tópico 2  "),
+//        child: Center(
+//          child: Text("Home Page"),
+//        ),
       ),
       // body: RefreshIndicator(
       //   child: ListView(
@@ -31,5 +37,9 @@ class HomePage extends StatelessWidget {
         //   ],
         // ),
     );
+  }
+  
+  Widget markdown(String data) {
+    return Markdown(data: data);
   }
 }

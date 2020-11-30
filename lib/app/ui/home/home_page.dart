ba4fb0bc0449f21311b4a2605bdfb126a2b25b47
Lucas de Castro Lopes <lucascastro@id.uff.br>
2020-11-30 20:07:35 -0300

    [WIP] Bottom navigation bar

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -1,13 +1,19 @@
 import 'package:araci/app/controller/home_controller.dart';
+import 'package:araci/app/data/provider/api.dart';
 import 'package:araci/app/data/repository/home_repository.dart';
-import 'package:araci/app/ui/home/widgets/card_widget.dart';
-import 'package:araci/app/ui/home/widgets/markdown_widget.dart';
-import 'package:araci/app/ui/home/widgets/video_widget.dart';
+import 'package:araci/app/ui/home/widgets/bottom_navigation_bar_widget.dart';
+import 'package:araci/app/ui/home/widgets/list_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class HomePage extends StatelessWidget {
+  int _activeIndex = 0;
+  final _widgetOptions = [
+    Container(),
+    Container(),
+    Container()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,24 +25,19 @@ class HomePage extends StatelessWidget {
         builder:(_) => RefreshIndicator(
           child: Column(
             children: [
-              Expanded(
-                child: ListView(
-                  children: [
-                    homeCardWidget("Card 1", ()=> Get.snackbar("Card 1", "Card 1 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-                    homeCardWidget("Card 2", ()=> Get.snackbar("Card 2", "Card 2 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-                    homeCardWidget("Card 3", ()=> Get.snackbar("Card 3", "Card 3 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-                    homeCardWidget("Card 4", ()=> Get.snackbar("Card 4", "Card 4 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-                    homeCardWidget("Card 5", ()=> Get.snackbar("Card 5", "Card 5 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-                    homeCardWidget("Card 6", ()=> Get.snackbar("Card 6", "Card 6 selecionado", snackPosition: SnackPosition.BOTTOM), context)
-                  ],
-                ),
-              )
+              buildList(context: context),
             ],
           ),
           onRefresh: _.updateVidsList,
         ),
       ),
+//      bottomNavigationBar: bottomNavigationBar([Icons.home, Icons.list, Icons.person],
+//          _activeIndex, _onItemTapped)
     );
   }
-  
+
+  ///CORRIGIR ISSO AQUI
+  void _onItemTapped() {
+    ///TODO: implementação do onTap do navigationBar
+  }
 }

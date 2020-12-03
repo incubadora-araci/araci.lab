de5d9a4be159b708be9bac9cd365feaa21a9c321
Lucas de Castro Lopes <lucascastro@id.uff.br>
2020-12-03 17:35:22 -0300

    [WIP] Testing images in cards

--- lib/app/ui/home/widgets/list_widget.dart
+++ lib/app/ui/home/widgets/list_widget.dart
@@ -6,12 +6,12 @@ Widget buildList({String key, String string, BuildContext context}) {
   return Expanded(
     child: ListView(
       children: [
-        homeCardWidget("Card 1", ()=> Get.snackbar("Card 1", "Card 1 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-        homeCardWidget("Card 2", ()=> Get.snackbar("Card 2", "Card 2 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-        homeCardWidget("Card 3", ()=> Get.snackbar("Card 3", "Card 3 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-        homeCardWidget("Card 4", ()=> Get.snackbar("Card 4", "Card 4 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-        homeCardWidget("Card 5", ()=> Get.snackbar("Card 5", "Card 5 selecionado", snackPosition: SnackPosition.BOTTOM), context),
-        homeCardWidget("Card 6", ()=> Get.snackbar("Card 6", "Card 6 selecionado", snackPosition: SnackPosition.BOTTOM), context)
+        homeCardWidget("Cosme Corrêa", "assets/testImages/CosmeCoffee.png", ()=> Get.snackbar("Card 1", "Card 1 selecionado", snackPosition: SnackPosition.BOTTOM), context),
+        homeCardWidget("Volta Redonda", "assets/testImages/VR.jpg", ()=> Get.snackbar("Card 2", "Card 2 selecionado", snackPosition: SnackPosition.BOTTOM), context),
+//        homeCardWidget("Card 3", ()=> Get.snackbar("Card 3", "Card 3 selecionado", snackPosition: SnackPosition.BOTTOM), context),
+//        homeCardWidget("Card 4", ()=> Get.snackbar("Card 4", "Card 4 selecionado", snackPosition: SnackPosition.BOTTOM), context),
+//        homeCardWidget("Card 5", ()=> Get.snackbar("Card 5", "Card 5 selecionado", snackPosition: SnackPosition.BOTTOM), context),
+//        homeCardWidget("Card 6", ()=> Get.snackbar("Card 6", "Card 6 selecionado", snackPosition: SnackPosition.BOTTOM), context)
       ],
     ),
   );

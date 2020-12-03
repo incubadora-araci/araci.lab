4b33fb1bae9bd4e0f4e09a77bd3a4ea22fc4ffc2
Ronald M Sampaio <ronald@localhost.localdomain>
2020-12-03 15:58:54 -0300

    novos repository

---
+++ lib/app/data/repository/article_repository.dart
@@ -0,0 +1,11 @@
+import 'package:araci/app/data/provider/databaseApi.dart';
+import 'package:meta/meta.dart';
+
+class ArticleRepository {
+  final DatabaseApi databaseApi;
+
+  ArticleRepository({@required this.databaseApi}) : assert(databaseApi != null);
+
+//TODO: implement get methods
+}
+
---
+++ lib/app/data/repository/globalInformation_repository.dart
@@ -0,0 +1,12 @@
+import 'package:araci/app/data/provider/global_information.dart';
+import 'package:meta/meta.dart';
+
+class GlobalInformationRepository {
+  final GlobalInformationApi globalInformationApi;
+
+  GlobalInformationRepository({@required this.globalInformationApi}) : assert(globalInformationApi != null);
+
+
+  //TODO: Implement get data
+}
+

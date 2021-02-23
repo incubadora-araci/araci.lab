7eaa0573a86e8933f978e7eda0b94e5604dc1ab0
Ronald M <ronaldmaymone@id.uff.br>
2021-02-23 15:42:34 -0300

    [0.8.0] integration with googleSheet

--- lib/app/data/database/const.dart
+++ lib/app/data/database/const.dart
@@ -13,42 +13,6 @@ const List<String> TABLES_MODELS = [
     relatedIds text DEFAULT NULL
   );
   """
-  // """
-  // CREATE TABLE Article (
-  //   id integer NOT NULL CONSTRAINT Node_pk PRIMARY KEY,
-  //   content text NOT NULL,
-  //   title text NOT NULL
-  // );
-  // """,
-  // """
-  // CREATE TABLE Image (
-  //   id integer NOT NULL CONSTRAINT Image_pk PRIMARY KEY,
-  //   file blob NOT NULL,
-  //   article_id integer NOT NULL,
-  //   CONSTRAINT Image_Article FOREIGN KEY (article_id)
-  //   REFERENCES Article (id)
-  // );
-  // """,
-  // """
-  // CREATE TABLE Link (
-  //   id integer NOT NULL CONSTRAINT Link_pk PRIMARY KEY,
-  //   url text NOT NULL,
-  //   article_id integer NOT NULL,
-  //   CONSTRAINT Link_Article FOREIGN KEY (article_id)
-  //   REFERENCES Article (id)
-  // );
-  // """,
-  //
-  // """
-  // CREATE TABLE Video (
-  //   id integer NOT NULL CONSTRAINT Video_pk PRIMARY KEY,
-  //   url text NOT NULL,
-  //   title text NOT NULL,
-  //   article_id integer NOT NULL,
-  //   CONSTRAINT Video_Article FOREIGN KEY (article_id)
-  //   REFERENCES Article (id)
-  // );
-  // """,
 ];
 
 const Map<String, List<String>> TABLES_MODELS_UPDATES = {

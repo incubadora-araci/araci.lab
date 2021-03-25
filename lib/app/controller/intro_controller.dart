225117b6950a5de63a47b4705a154b9de1ae6929
Ronald M <ronaldmaymone@id.uff.br>
2021-03-25 10:15:31 -0300

    [0.10.1] mudança no texto da intro_page

--- lib/app/controller/intro_controller.dart
+++ lib/app/controller/intro_controller.dart
@@ -22,14 +22,14 @@ class IntroController extends GetxController {
     Get.toNamed(Routes.DETAILS);
   }
 
-  void createList() async {
+  Future createList() async {
     listPagesViewModel.add(
         PageViewModel(
-          title: "Bem-vindo ao projeto Araci!",
+          title: "Bem-vindo(a) ao projeto Araci.lab!",
           bodyWidget: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae feugiat enim. Fusce nec dui porttitor augue sodales imperdiet. Quisque commodo lorem ut rhoncus auctor. Nunc vel diam porttitor, lacinia lorem id, suscipit sapien. Curabitur sit amet dui eu lectus tempus sollicitudin ac eu quam. Maecenas molestie est molestie, tincidunt augue id, consequat risus. Cras at nibh eu quam ornare condimentum sit amet et elit. Aenean congue, diam imperdiet condimentum auctor, velit libero ullamcorper lectus, id consequat risus mauris a purus. Aliquam pharetra ultrices lacus, a rhoncus nulla. Nullam faucibus ex quis sem rhoncus, a facilisis mi sagittis. Sed vehicula massa nulla, et consequat purus tristique non. Phasellus aliquet sed tellus aliquet ullamcorper. ", overflow: TextOverflow.visible)),
+              Expanded(child: Text("Araci.lab é um braço da Araci Incubadora de Projetos de Cinema e Audiovisual voltado para a formação. A proposta é promover encontros entre estudantes e profissionais, realizar oficinas de desenvolvimento de projetos e oferecer cursos de curta duração voltados para o trabalho audiovisual. Entre os objetivos da Araci estão a profissionalização dos graduandos, a divulgação e aplicabilidade da pesquisa científica do campo e a formação continuada dos egressos de cinema e audiovisual. A ideia é a criação de um repositório digital para que todos tenham acesso às ações da Araci.lab e possam consultar a qualquer momento a partir de seus telefones celulares.  ", overflow: TextOverflow.visible)),
 
             ],
           ),

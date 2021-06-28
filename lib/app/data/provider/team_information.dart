adfdd37a10fcd8077d9aac1e373a26c42763a94b
Ronald M <ronaldmaymone@id.uff.br>
2021-06-28 09:47:58 -0300

    [1.1.1] texto Tereza

--- lib/app/data/provider/team_information.dart
+++ lib/app/data/provider/team_information.dart
@@ -4,25 +4,25 @@ class TeamInformation{
   static const List<Map<String, dynamic>> team = [
     {
       'name':'Elianne Ivo Barroso',
-      'role': '#### Professora Associada do Departamento de Cinema e Vídeo.\n\n#### Coordenadora da Araci Incubadora de Projetos de Cinema e Audiovisual. PPGCINE/PPGCMC da UFRJ',
+      'role': '#### Professora Associada do Departamento de Cinema e Vídeo.\n\n#### Coordenadora da Araci Incubadora de Projetos de Cinema e Audiovisual. PPGCINE/PPGCMC da UFRJ.',
       'imgPath': 'assets/images/team/ElianneProfilePic.jpg',
       'squared': false
     },
     {
       'name': 'Tereza Cristina N. Machado',
-      'role': '#### Administradora',
+      'role': '#### Administradora.\n#### Coordenadora Administrativa do Laboratorio Integrado de Artes (LIA-IACS).\n#### Doutora em Educação.\n#### Ex Superintendente do Centro de Artes UFF (2006-2014).\n#### Idealizadora do Aplicativo Araci.Lab.',
       'imgPath': 'assets/images/team/TerezaProfilePic.jpg',
       'squared': false
     },
     {
       'name': 'Alunos',
-      'role': '#### **Banco de talentos** : Camila Garritano e Maria Eduarda D\'Elia\n\n #### **Idealização do Aplicativo** : Darcy Tenório e Maria Laura Rodrigues ',
+      'role': '#### **Banco de talentos** : Camila Garritano e Maria Eduarda D\'Elia.\n\n #### **Idealização do Aplicativo** : Darcy Tenório e Maria Laura Rodrigues.',
       'imgPath': null,
       'squared': false
     },
     {
       'name': 'STI Mobile',
-      'role': '#### **Gestão de Projetos**: Cosme Faria Corrêa\n\n**Desenvolvedores**: Ronald Maymone Sampaio e Paulo Rodrigo L. Figueiredo\n\n**Contribuição**: Lucas de Castro Lopes',
+      'role': '#### **Gestão de Projetos**: Cosme Faria Corrêa\n\n**Desenvolvimento**: Ronald Maymone Sampaio e Paulo Rodrigo L. Figueiredo.\n\n**Contribuição**: Lucas de Castro Lopes.',
       'imgPath': 'assets/images/team/STI-Mobile.png',
       'squared': true
     },
